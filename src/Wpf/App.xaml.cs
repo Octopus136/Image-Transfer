845577cc5d8b55b0b4bd792e3b345ed81fc01dce
_Burnside <1361227324@qq.com>
2025-12-05 17:30:51 +0800

    i18n: add zh-TW and en-US localization support

--- src/Wpf/App.xaml.cs
+++ src/Wpf/App.xaml.cs
@@ -1,9 +1,36 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System;
+using System.Windows;
 
 namespace ImgSizer.Wpf
 {
+    public class LanguageItem
+    {
+        public string DisplayName { get; set; } = "";
+        public string Code { get; set; } = "";
+    }
     public partial class App : Application
     {
+        public void ChangeLanguage(string langCode)
+        {
+            string fileName = $"Localizations/{langCode.ToLower()}.xaml";
+
+            var newDict = new ResourceDictionary
+            {
+                Source = new Uri(fileName, UriKind.Relative)
+            };
+
+            var mergedDicts = Resources.MergedDictionaries;
+            var existingDicts = mergedDicts
+                .Where(d => d.Source != null &&
+                            d.Source.OriginalString.StartsWith("Localizations/", StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            foreach (var dict in existingDicts)
+                mergedDicts.Remove(dict);
+
+            mergedDicts.Add(newDict);
+        }
     }
 
 }
--- src/Wpf/ViewModels/RootView.cs
+++ src/Wpf/ViewModels/RootView.cs
@@ -1,10 +1,33 @@
 ﻿using Stylet;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace ImgSizer.Wpf
 {
     // 负责整体窗口 & 模式切换
     public class RootViewModel : Conductor<Screen>.Collection.OneActive
     {
+        public ObservableCollection<LanguageItem> Languages { get; } =
+        new ObservableCollection<LanguageItem>
+        {
+            new LanguageItem { DisplayName = "简体中文", Code = "zh-CN" },
+            new LanguageItem { DisplayName = "繁體中文", Code = "zh-TW" },
+            new LanguageItem { DisplayName = "English", Code = "en-US" },
+        };
+
+        private LanguageItem _selectedLanguage;
+        public LanguageItem SelectedLanguage
+        {
+            get => _selectedLanguage;
+            set
+            {
+                SetAndNotify(ref _selectedLanguage, value);
+                if (value != null)
+                {
+                    ((App)Application.Current).ChangeLanguage(value.Code);
+                }
+            }
+        }
         public SingleConvertViewModel SingleConvert { get; }
 
         public AdvancedOptionsViewModel AdvancedOptions { get; }
@@ -21,6 +44,7 @@ namespace ImgSizer.Wpf
 
             // 默认显示单图
             ActiveItem = SingleConvert;
+            SelectedLanguage = Languages[0]; // 默认中文
         }
 
         public void ShowSingle()
